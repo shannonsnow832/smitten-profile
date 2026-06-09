0201ee7aecb40ea996280227e6c29e59ba558c14
shannonsnow832 <shannonsnow832@gmail.com>
2026-06-09 16:55:49 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', function(e) {});
